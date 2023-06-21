171195f68a77d1c154fd43b91cc331433a591c92
ParamonovIvan <131868856+ParamonovIvan@users.noreply.github.com>
2023-06-21 10:39:57 +0400

    Add files via upload

---
+++ Задание 4/js.js
@@ -0,0 +1,32 @@
+﻿
+const inputWidthNode = document.querySelector('.width');
+const inputHeightNode = document.querySelector('.height');
+const btnNode = document.querySelector('.submit');
+const resultNode = document.querySelector('.result');
+
+const useRequest = (value1, value2) => {
+    return fetch(`https://picsum.photos/${value1}/${value2}`)
+        .then((response) => {
+            return response;
+        })
+        .then(data => data.url)
+        .catch(() => {
+            console.log('error')
+        });
+};
+
+btnNode.addEventListener('click', async () => {
+  value1 = Number(inputWidthNode.value);
+  value2 = Number(inputHeightNode.value);
+  if (value1 < 100 || value1 > 300 || isNaN(value1) || value2 < 100 || value2 > 300 || isNaN(value2)) {
+    resultNode.textContent = 'Одно из чисел вне диапазона от 100 до 300';
+  } else {
+      const requestResult = await useRequest(value1, value2);
+      image = document.createElement('img');
+      image.src = requestResult;
+      resultNode.append(image);
+    }
+});
+
+
+
